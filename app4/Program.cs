8f7fd8cfde9306732e788d5d11fab7cd9e20205d
Ziya Suzen <ziya@suzen.net>
2023-05-14 13:06:49 +0100

    :zap:

--- app4/Program.cs
+++ app4/Program.cs
@@ -1,11 +1,14 @@
 ﻿using NATS.Client.Core;
 
+Console.Error.WriteLine($"Starting..");
+
+Console.Error.WriteLine($"Connection ready");
+
 await using var connection = new NatsConnection();
 await connection.XxxPingAsync();
 
-Console.ReadLine();
+Console.Error.WriteLine($"Sub1");
 
-Console.Error.WriteLine("SUB1======================================================");
 await connection.XxxSubscribeAsync<byte[]>("foo", m=>
 {
     Console.Error.Write("[CALLBACK-1] RCV: ");
@@ -16,7 +19,7 @@ await connection.XxxSubscribeAsync<byte[]>("foo", m=>
     Console.Error.WriteLine();
 });
 
-Console.ReadLine();
+Console.Error.WriteLine($"Sub2");
 
 Console.Error.WriteLine("SUB2======================================================");
 await connection.XxxSubscribeAsync<byte[]>("foo", m =>
@@ -32,27 +35,21 @@ await connection.XxxSubscribeAsync<byte[]>("foo", m =>
 byte[] array = { 65, 66, 67 };
 ReadOnlyMemory<byte> readOnlyMemory = new ReadOnlyMemory<byte>(array);
 NatsKey natsKey = new NatsKey("foo");
-for (int i = 0; i < 100; i++)
+for (int j = 0; j < 3; j++)
 {
-    Console.Error.WriteLine("PUB=============================================");
-    await connection.XxxPublishAsync(natsKey, readOnlyMemory);
+    Console.Error.WriteLine($"Pub{j}");
+
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Error.WriteLine("PUB=============================================");
+        await connection.XxxPublishAsync(natsKey, readOnlyMemory);
+        Thread.Sleep(10);
+    }
+    
+    Console.Error.WriteLine($"Take{j}...");
     Console.ReadLine();
 }
 
 
-for (int i = 0; i < 10; i++)
-{
-    var timeSpan = await connection.XxxPingAsync();
-    Console.WriteLine($"ping:{timeSpan}");
-}
+Console.Error.WriteLine($"Bye");
 
-for (int i = 0; i < 10; i++)
-{
-    await connection.XxxPublishAsync("foo", $"bla-{i}").ConfigureAwait(false);
-}
-
-await connection.XxxPublishAsync("foo", "bla2").ConfigureAwait(false);
-
-Console.ReadKey();
-
-Console.WriteLine("exit");
