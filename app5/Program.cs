78fa593aef7e097071e86861bdd89cf684c4a47f
Ziya Suzen <ziya@suzen.net>
2023-07-11 21:03:31 +0100

    :zap:2

--- app5/Program.cs
+++ app5/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hi!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hi!");
+        var obj = new My();
+        if (obj is IMy imy)
+        {
+            Console.WriteLine($"IMy:{imy.GetType().Name}");
+        }
+        if (obj is MyAbs myAbs)
+        {
+            Console.WriteLine($"MyAbs:{myAbs.GetType().Name}");
+        }
+    }
+}
+
+interface IMy{}
+abstract class MyAbs : IMy {}
+class My : MyAbs {}
