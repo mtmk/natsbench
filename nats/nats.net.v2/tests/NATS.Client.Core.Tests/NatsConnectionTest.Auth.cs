86f575448e1646d9d854681281458b4b6f1b5577
Ziya Suzen <ziya@suzen.net>
2023-08-09 16:19:52 +0100

    :zap:1

--- nats/nats.net.v2/tests/NATS.Client.Core.Tests/NatsConnectionTest.Auth.cs
+++ nats/nats.net.v2/tests/NATS.Client.Core.Tests/NatsConnectionTest.Auth.cs
@@ -95,6 +95,7 @@ public abstract partial class NatsConnectionTest
 
         var serverOptions = new NatsServerOptionsBuilder()
             .UseTransport(_transportType)
+            .Trace()
             .AddServerConfig(serverConfig)
             .Build();
 
