39fc862985df46401ca6822b007c678cbf5fcc8e
Ziya Suzen <ziya@suzen.net>
2023-08-15 23:44:06 +0100

    :zap:1

--- nnats-proxy/Program.cs
+++ nnats-proxy/Program.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Concurrent;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 
 public class Program
@@ -21,7 +20,8 @@ public class Program
                      drop <client-id>   Close TCP connection of client
                      ctrl               Toggle displaying core control messages
                      js                 Toggle JetStream summarization
-                     js-hb              Toggle suppressing JetStream Heartbeat messages
+                     js-hb              Toggle displaying JetStream Heartbeat messages
+                     js-shb             Toggle suppressing JetStream Heartbeat messages
                      js-msg             Toggle displaying JetStream messages
                      q, quit            Quit program and stop nats-server
 
@@ -119,19 +119,23 @@ public class Program
             }
             else if (Regex.IsMatch(cmd, @"^\s*(ctrl)\s*$"))
             {
-                server.DisplayCtrl = !server.DisplayCtrl;
+                server.DisplayCtrl.Toggle();
             }
             else if (Regex.IsMatch(cmd, @"^\s*(js)\s*$"))
             {
-                server.JetStreamSummarization = !server.JetStreamSummarization;
+                server.JetStreamSummarization.Toggle();
+            }
+            else if (Regex.IsMatch(cmd, @"^\s*(js-shb)\s*$"))
+            {
+                server.SuppressHeartbeats.Toggle();
             }
             else if (Regex.IsMatch(cmd, @"^\s*(js-hb)\s*$"))
             {
-                server.SuppressHeartbeats = !server.SuppressHeartbeats;
+                server.DisplayHeartbeats.Toggle();
             }
             else if (Regex.IsMatch(cmd, @"^\s*(js-msg)\s*$"))
             {
-                server.DisplayMessages = !server.DisplayMessages;
+                server.DisplayMessages.Toggle();
             }
             else if (cmd.StartsWith("drop"))
             {
@@ -145,6 +149,8 @@ public class Program
                 {
                     Console.WriteLine($"Error: Can't parse drop command: {cmd}");
                 }
+
+                prompt = false;
             }
             else
             {
@@ -155,214 +161,23 @@ public class Program
 
     static void SetupSocket(Socket socket)
     {
-        try
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            socket.SendBufferSize = 0;
-            socket.ReceiveBufferSize = 0;
-        }
-        catch
-        {
-            /*ignore*/
+            try
+            {
+                socket.SendBufferSize = 0;
+                socket.ReceiveBufferSize = 0;
+            }
+            catch
+            {
+                /*ignore*/
+            }
         }
 
         socket.NoDelay = true;
     }
 
     private static int _client = 0;
-
-    class Server
-    {
-        private readonly ConcurrentDictionary<int, TcpClient> _clients = new();
-
-        private int _js;
-        public bool JetStreamSummarization
-        {
-            get => Volatile.Read(ref _js) == 1;
-            set => Interlocked.Exchange(ref _js, value ? 1 : 0);
-        }
-
-        private int _shb;
-        public bool SuppressHeartbeats
-        {
-            get => Volatile.Read(ref _shb) == 1;
-            set => Interlocked.Exchange(ref _shb, value ? 1 : 0);
-        }
-
-        private int _msg = 1;
-        public bool DisplayMessages
-        {
-            get => Volatile.Read(ref _msg) == 1;
-            set => Interlocked.Exchange(ref _msg, value ? 1 : 0);
-        }
-
-        private int _ctrl;
-        public bool DisplayCtrl
-        {
-            get => Volatile.Read(ref _ctrl) == 1;
-            set => Interlocked.Exchange(ref _ctrl, value ? 1 : 0);
-        }
-
-        public void NewClient(int id, TcpClient tcpClient)
-        {
-            _clients[id] = tcpClient;
-        }
-
-        public void Drop(int id)
-        {
-            Console.WriteLine($"Dropping client [{id}]...");
-            if (_clients.TryGetValue(id, out var client))
-            {
-                try
-                {
-                    client.Close();
-                    Console.WriteLine($"Client [{id}] connection closed.");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Error: closing client [{id}]: {e.GetBaseException().Message}");
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Error: Can't find client [{id}]");
-            }
-        }
-
-        public void Print(string dir, string line, string ascii)
-        {
-            try
-            {
-                if (JetStreamSummarization)
-                {
-                    string? js = null;
-                    var skip = false;
-                    Match m1;
-                    Match m2;
-                    if ((m1 = Regex.Match(ascii, @"100 Idle Heartbeat\\r\\nNats-Last-Consumer: (\d+)\\r\\nNats-Last-Stream: (\d+)")).Success)
-                    {
-                        // NATS/1.0 100 Idle Heartbeat\r\nNats-Last-Consumer: 107\r\nNats-Last-Stream: 107\r\n\r\n
-                        var lastConsumer = m1.Groups[1].Value;
-                        var lastStream = m1.Groups[2].Value;
-                        js = $"[C] [IHB] consumer:{lastConsumer} stream:{lastStream}";
-                    }
-                    //else if ((m1 = Regex.Match(buffer, @"")).Success)
-                    else if ((m1 = Regex.Match(ascii, @"408 Request Timeout\\r\\nNats-Pending-Messages: (\d+)\\r\\nNats-Pending-Bytes: (\d+)")).Success)
-                    {
-                        // NATS/1.0 408 Request Timeout\r\nNats-Pending-Messages: 10\r\nNats-Pending-Bytes: 0\r\n\r\n
-                        var pendingMsgs = m1.Groups[1].Value;
-                        var pendingBytes = m1.Groups[2].Value;
-                        js = $"[C] [RTO] pending msgs:{pendingMsgs} bytes:{pendingBytes}";
-                    }
-                    else if ((m1 = Regex.Match(line, @"^PUB \$JS\.API\.CONSUMER\.MSG\.NEXT\.(\w+)\.(\w+)")).Success)
-                    {
-                        // PUB $JS.API.CONSUMER.MSG.NEXT.s1.c2 _INBOX.82YIGQ8W3TI0XNGETMHF03 77
-                        // {"batch":10,"max_bytes":0,"idle_heartbeat":15000000000,"expires":30000000000}
-                        var stream = m1.Groups[1].Value;
-                        var consumer = m1.Groups[2].Value;
-                        var json = JsonNode.Parse(ascii);
-                        var batch = json["batch"]?.GetValue<int>();
-                        js = $"[C] [NXT] {stream}/{consumer} batch:{batch}";
-                    }
-                    else if (
-                        (m1 = Regex.Match(line, @"^PUB \$JS\.ACK\.(\w+)\.(\w+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)")).Success
-                        && (m2 = Regex.Match(ascii, @"^\+ACK$")).Success)
-                    {
-                        if (DisplayMessages)
-                        {
-                            // PUB $JS.ACK.s1.c2.1.119.119.1692099975248777900.0 4
-                            //+ACK
-                            var stream = m1.Groups[1].Value;
-                            var consumer = m1.Groups[2].Value;
-                            var v1 = ulong.Parse(m1.Groups[3].Value);
-                            var v2 = ulong.Parse(m1.Groups[4].Value);
-                            var v3 = ulong.Parse(m1.Groups[5].Value);
-                            var v4 = ulong.Parse(m1.Groups[6].Value);
-                            var v5 = ulong.Parse(m1.Groups[7].Value);
-                            js = $"[M] [ACK] {stream}/{consumer} v1:{v1} v2:{v2} v3:{v3} v4:{v4} v5:{v5}";
-                        }
-                        else
-                        {
-                            js = "";
-                            skip = true;
-                        }
-                    }
-                    else if ((m1 = Regex.Match(line, @"^MSG (\S+) (\S+) \$JS\.ACK\.(\w+)\.(\w+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)")).Success)
-                    {
-                        if (DisplayMessages)
-                        {
-                            // MSG s1.x 2 $JS.ACK.s1.c2.1.130.130.1692100700116417900.0 17
-                            // AAAAAAAAAAAAAAAAA
-                            var subject = m1.Groups[1].Value;
-                            var sid = m1.Groups[2].Value;
-                            var stream = m1.Groups[3].Value;
-                            var consumer = m1.Groups[4].Value;
-                            var v1 = ulong.Parse(m1.Groups[5].Value);
-                            var v2 = ulong.Parse(m1.Groups[6].Value);
-                            var v3 = ulong.Parse(m1.Groups[7].Value);
-                            var v4 = ulong.Parse(m1.Groups[8].Value);
-                            var v5 = ulong.Parse(m1.Groups[9].Value);
-                            js = $"[M] [MSG] {subject} {stream}/{consumer} v1:{v1} v2:{v2} v3:{v3} v4:{v4} v5:{v5}"
-                                // + $"\n    {new string(' ', dir.Length)} {ascii}"
-                                ;
-                        }
-                        else
-                        {
-                            js = "";
-                            skip = true;
-                        }
-                    }
-                    
-                    if (js != null)
-                    {
-                        if (!skip)
-                        {
-                            Console.WriteLine($"{dir} JS {DateTime.Now:HH:mm:ss} {js}");
-                        }
-
-                        return;
-                    }
-                }
-
-                Console.WriteLine($"{dir} {line}\n{new string(' ', dir.Length)} {ascii}\n");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"");
-                Console.WriteLine($"Print error :{e.Message}");
-                Console.WriteLine($"  dir:{dir}");
-                Console.WriteLine($"  line:{line}");
-                Console.WriteLine($"  buffer:{ascii}");
-                Console.WriteLine($"");
-            }
-        }
-
-        public void Write(TextWriter sw, string dir, string line, char[] buffer, string ascii)
-        {
-            Print(dir, line, ascii);
-            if (SuppressHeartbeats && Regex.IsMatch(ascii, @"100 Idle Heartbeat"))
-            {
-                Console.WriteLine($"{new string(' ', dir.Length)}     [SUPPRESSED]");
-            }
-            else
-            {
-                sw.WriteLine(line);
-                sw.Write(buffer);
-                sw.Flush();
-            }
-        }
-
-        public void WriteCtrl(TextWriter sw, string dir, string line)
-        {
-            // Ignore control messages
-            if (DisplayCtrl && !Regex.IsMatch(line, @"^(INFO|CONNECT|PING|PONG|\+OK)"))
-            {
-                Console.WriteLine($"{dir} {line}");
-            }
-
-            sw.WriteLine(line);
-            sw.Flush();
-        }
-    }
     
     static void RunServer(Server server, ManualResetEventSlim started, IPAddress address, int proxyPort, int serverPort)
     {
@@ -392,25 +207,44 @@ public class Program
 #pragma warning disable CS4014
             Task.Run(() =>
             {
-                var clientStream = clientTcpConnection.GetStream();
-                var csr = new StreamReader(clientStream, Encoding.ASCII);
-                var csw = new StreamWriter(clientStream, Encoding.ASCII);
-
-                var serverStream = serverTcpConnection.GetStream();
-                var ssr = new StreamReader(serverStream, Encoding.ASCII);
-                var ssw = new StreamWriter(serverStream, Encoding.ASCII);
-
-                Task.Run(() =>
+                try
                 {
-                    // Client -> Server
-                    while (NatsProtoDump(server, $"[{n}] -->", csr, ssw))
+                    var clientStream = clientTcpConnection.GetStream();
+                    var csr = new StreamReader(clientStream, Encoding.ASCII);
+                    var csw = new StreamWriter(clientStream, Encoding.ASCII);
+
+                    var serverStream = serverTcpConnection.GetStream();
+                    var ssr = new StreamReader(serverStream, Encoding.ASCII);
+                    var ssw = new StreamWriter(serverStream, Encoding.ASCII);
+
+                    Task.Run(() =>
+                    {
+                        try
+                        {
+                            // Client -> Server
+                            while (NatsProtoDump(server, $"[{n}] -->", csr, ssw))
+                            {
+                            }
+
+                            ssr.Close();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"[LOOP3] {e.GetType()}: {e.Message}");
+                        }
+                    });
+
+                    // Server -> client
+                    while (NatsProtoDump(server, $"[{n}] <--", ssr, csw))
                     {
                     }
-                });
-                
-                // Server -> client
-                while (NatsProtoDump(server, $"[{n}] <--", ssr, csw))
+
+                    ssr.Close();
+                    csr.Close();
+                }
+                catch (Exception e)
                 {
+                    Console.WriteLine($"[LOOP1] {e.GetType()}: {e.Message}");
                 }
             });
         }
@@ -418,64 +252,76 @@ public class Program
 
     static bool NatsProtoDump(Server server, string dir, StreamReader sr, StreamWriter sw)
     {
-        var line = sr.ReadLine();
-        if (line == null) return false;
-
-        if (Regex.IsMatch(line, @"^(INFO|CONNECT|PING|PONG|UNSUB|SUB|\+OK|-ERR)"))
+        try
         {
-            server.WriteCtrl(sw, dir, line);
-            return true;
-        }
+            var line = sr.ReadLine();
+            if (line == null) return false;
 
-
-        var match = Regex.Match(line, @"^(?:PUB|HPUB|MSG|HMSG).*?(\d+)\s*$");
-        if (match.Success)
-        {
-            var size = int.Parse(match.Groups[1].Value);
-            var buffer = new char[size + 2];
-            var span = buffer.AsSpan();
-            while (true)
+            if (Regex.IsMatch(line, @"^(INFO|CONNECT|PING|PONG|UNSUB|SUB|\+OK|-ERR)"))
             {
-                var read = sr.Read(span);
-                if (read == 0) break;
-                if (read == -1) return false;
-                span = span[read..];
+                server.WriteCtrl(sw, dir, line);
+                return true;
             }
 
-            var ascii = new StringBuilder();
-            foreach (var c in buffer.AsSpan()[..size])
+
+            var match = Regex.Match(line, @"^(?:PUB|HPUB|MSG|HMSG).*?(\d+)\s*$");
+            if (match.Success)
             {
-                switch (c)
+                var size = int.Parse(match.Groups[1].Value);
+                var buffer = new char[size + 2];
+                var span = buffer.AsSpan();
+                while (true)
                 {
-                    case > ' ' and <= '~':
-                        ascii.Append(c);
-                        break;
-                    case ' ':
-                        ascii.Append(' ');
-                        break;
-                    case '\t':
-                        ascii.Append("\\t");
-                        break;
-                    case '\n':
-                        ascii.Append("\\n");
-                        break;
-                    case '\r':
-                        ascii.Append("\\r");
-                        break;
-                    default:
-                        ascii.Append('.');
-                        // sb.Append(Convert.ToString(c, 16));
-                        break;
+                    var read = sr.Read(span);
+                    if (read == 0) break;
+                    if (read == -1) return false;
+                    span = span[read..];
                 }
+
+                var ascii = new StringBuilder();
+                foreach (var c in buffer.AsSpan()[..size])
+                {
+                    switch (c)
+                    {
+                        case > ' ' and <= '~':
+                            ascii.Append(c);
+                            break;
+                        case ' ':
+                            ascii.Append(' ');
+                            break;
+                        case '\t':
+                            ascii.Append("\\t");
+                            break;
+                        case '\n':
+                            ascii.Append("\\n");
+                            break;
+                        case '\r':
+                            ascii.Append("\\r");
+                            break;
+                        default:
+                            ascii.Append('.');
+                            // sb.Append(Convert.ToString(c, 16));
+                            break;
+                    }
+                }
+
+                server.Write(sw, dir, line, buffer, ascii.ToString());
+
+                return true;
             }
 
-            server.Write(sw, dir, line, buffer, ascii.ToString());
-            
-            return true;
+            Console.WriteLine($"Error: Unknown protocol: {line}");
+
+            return false;
         }
-
-        Console.WriteLine($"Error: Unknown protocol: {line}");
-
-        return false;
+        catch (IOException)
+        {
+            return false;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{dir} {e.GetType()}: {e.Message}");
+            return false;
+        }
     }
 }
---
+++ nnats-proxy/Server.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections.Concurrent;
+using System.Net.Sockets;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+
+#pragma warning disable CS4014
+public class Server
+{
+    public class AtomicBool
+    {
+        private int _i;
+
+        public AtomicBool(bool value) => _i = value ? 1 : 0;
+
+        public void Set() => Interlocked.Exchange(ref _i, 1);
+        
+        public void Reset() => Interlocked.Exchange(ref _i, 0);
+
+        public void Toggle()
+        {
+            if (True) Reset();
+            else Set();
+        }
+
+        public bool True => Volatile.Read(ref _i) == 1;
+        
+        public static implicit operator bool(AtomicBool d) => d.True;
+        
+        public static explicit operator AtomicBool(bool b) => new(b);
+
+        public override string ToString() => True ? "ON" : "OFF";
+    }
+    
+    private readonly ConcurrentDictionary<int, TcpClient> _clients = new();
+
+    public AtomicBool JetStreamSummarization { get; } = new(false);
+    public AtomicBool SuppressHeartbeats { get; } = new(false);
+    public AtomicBool DisplayMessages { get; } = new(true);
+    public AtomicBool DisplayHeartbeats { get; } = new(true);
+    public AtomicBool DisplayCtrl { get; } = new(true);
+
+    public void NewClient(int id, TcpClient tcpClient)
+    {
+        _clients[id] = tcpClient;
+    }
+
+    public void Drop(int id)
+    {
+        Console.WriteLine($"Dropping client [{id}]...");
+        if (_clients.TryGetValue(id, out var client))
+        {
+            try
+            {
+                client.Close();
+                Console.WriteLine($"Client [{id}] connection closed.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: closing client [{id}]: {e.GetBaseException().Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Error: Can't find client [{id}]");
+        }
+    }
+
+    public void Print(string dir, string line, string ascii)
+    {
+        try
+        {
+            if (JetStreamSummarization)
+            {
+                string? js = null;
+                var skip = false;
+                Match m1;
+                Match m2;
+                if ((m1 = Regex.Match(ascii, @"100 Idle Heartbeat\\r\\nNats-Last-Consumer: (\d+)\\r\\nNats-Last-Stream: (\d+)")).Success)
+                {
+                    if (DisplayHeartbeats)
+                    {
+                        // NATS/1.0 100 Idle Heartbeat\r\nNats-Last-Consumer: 107\r\nNats-Last-Stream: 107\r\n\r\n
+                        var lastConsumer = m1.Groups[1].Value;
+                        var lastStream = m1.Groups[2].Value;
+                        js = $"[C] HBT consumer:{lastConsumer} stream:{lastStream}";
+                    }
+                    else
+                    {
+                        js = "";
+                        skip = true;
+                    }
+                }
+                else if ((m1 = Regex.Match(ascii, @"408 Request Timeout\\r\\nNats-Pending-Messages: (\d+)\\r\\nNats-Pending-Bytes: (\d+)")).Success)
+                {
+                    // NATS/1.0 408 Request Timeout\r\nNats-Pending-Messages: 10\r\nNats-Pending-Bytes: 0\r\n\r\n
+                    var pendingMsgs = m1.Groups[1].Value;
+                    var pendingBytes = m1.Groups[2].Value;
+                    js = $"[C] RTO pending msgs:{pendingMsgs} bytes:{pendingBytes}";
+                }
+                else if ((m1 = Regex.Match(ascii, @"409 Message Size Exceeds MaxBytes\\r\\nNats-Pending-Messages: (\d+)\\r\\nNats-Pending-Bytes: (\d+)")).Success)
+                {
+                    // HMSG _INBOX.AZ6BFWO00BKRERV8NM9LS9 2 97 97
+                    // NATS/1.0 409 Message Size Exceeds MaxBytes\r\nNats-Pending-Messages: 99\r\nNats-Pending-Bytes: 46\r\n\r\n
+                    var pendingMsgs = m1.Groups[1].Value;
+                    var pendingBytes = m1.Groups[2].Value;
+                    js = $"[C] EXB pending msgs:{pendingMsgs} bytes:{pendingBytes}";
+                }
+                
+                else if ((m1 = Regex.Match(line, @"^PUB \$JS\.API\.CONSUMER\.MSG\.NEXT\.(\w+)\.(\w+)")).Success)
+                {
+                    // PUB $JS.API.CONSUMER.MSG.NEXT.s1.c2 _INBOX.82YIGQ8W3TI0XNGETMHF03 77
+                    // {"batch":10,"max_bytes":0,"idle_heartbeat":15000000000,"expires":30000000000}
+                    var stream = m1.Groups[1].Value;
+                    var consumer = m1.Groups[2].Value;
+                    var json = JsonNode.Parse(ascii);
+                    var batch = json["batch"]?.GetValue<int>();
+                    var maxBytes = json["max_bytes"]?.GetValue<int>();
+                    js = $"[C] NXT {stream}/{consumer} batch:{batch} max_bytes:{maxBytes}";
+                }
+                else if (
+                    (m1 = Regex.Match(line, @"^PUB \$JS\.ACK\.(\w+)\.(\w+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)")).Success
+                    && (m2 = Regex.Match(ascii, @"^\+ACK$")).Success)
+                {
+                    if (DisplayMessages.True)
+                    {
+                        // PUB $JS.ACK.s1.c2.1.119.119.1692099975248777900.0 4
+                        //+ACK
+                        var stream = m1.Groups[1].Value;
+                        var consumer = m1.Groups[2].Value;
+                        var v1 = ulong.Parse(m1.Groups[3].Value);
+                        var v2 = ulong.Parse(m1.Groups[4].Value);
+                        var v3 = ulong.Parse(m1.Groups[5].Value);
+                        var v4 = ulong.Parse(m1.Groups[6].Value);
+                        var v5 = ulong.Parse(m1.Groups[7].Value);
+                        js = $"[M] ACK {stream}/{consumer} v1:{v1} v2:{v2} v3:{v3} v4:{v4} v5:{v5}";
+                    }
+                    else
+                    {
+                        js = "";
+                        skip = true;
+                    }
+                }
+                else if ((m1 = Regex.Match(line, @"^MSG (\S+) (\S+) \$JS\.ACK\.(\w+)\.(\w+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)")).Success)
+                {
+                    if (DisplayMessages)
+                    {
+                        // MSG s1.x 2 $JS.ACK.s1.c2.1.130.130.1692100700116417900.0 17
+                        // AAAAAAAAAAAAAAAAA
+                        var subject = m1.Groups[1].Value;
+                        var sid = m1.Groups[2].Value;
+                        var stream = m1.Groups[3].Value;
+                        var consumer = m1.Groups[4].Value;
+                        var v1 = ulong.Parse(m1.Groups[5].Value);
+                        var v2 = ulong.Parse(m1.Groups[6].Value);
+                        var v3 = ulong.Parse(m1.Groups[7].Value);
+                        var v4 = ulong.Parse(m1.Groups[8].Value);
+                        var v5 = ulong.Parse(m1.Groups[9].Value);
+                        js = $"[M] MSG {subject} {stream}/{consumer} v1:{v1} v2:{v2} v3:{v3} v4:{v4} v5:{v5}"
+                            // + $"\n    {new string(' ', dir.Length)} {ascii}"
+                            ;
+                    }
+                    else
+                    {
+                        js = "";
+                        skip = true;
+                    }
+                }
+                else if ((m1 = Regex.Match(ascii, @"\{""type"":""io\.nats\.jetstream\.api\.v1\.([\w.]+)"",")).Success)
+                {
+                    //[9] <-- MSG _INBOX.W4KLXR004L8LA1ZT0DISVI.W4KLXR004L8LA1ZT0DISWW 1 550
+                    //{"type":"io.nats.jetstream.api.v1.consumer_info_response",...
+                    var type = m1.Groups[1].Value;
+                    var json = JsonNode.Parse(ascii);
+                    var error = json["error"]?.ToString();
+                    js = $"[C] API Response {type} {error}";
+                }
+                else if ((m1 = Regex.Match(line, @"^PUB \$JS\.API\.(\S+)")).Success)
+                {
+                    // PUB $JS.API.CONSUMER.MSG.NEXT.s1.c2 _INBOX.82YIGQ8W3TI0XNGETMHF03 77
+                    // {"batch":10,"max_bytes":0,"idle_heartbeat":15000000000,"expires":30000000000}
+                    var api = m1.Groups[1].Value;
+                    js = $"[C] API Request {api}";
+                }
+                    
+                if (js != null)
+                {
+                    if (!skip)
+                    {
+                        Console.WriteLine($"{dir} JS {DateTime.Now:HH:mm:ss} {js}");
+                    }
+
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{dir} {line}\n{new string(' ', dir.Length)} {ascii}\n");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"");
+            Console.WriteLine($"Print error :{e.Message}");
+            Console.WriteLine($"  dir:{dir}");
+            Console.WriteLine($"  line:{line}");
+            Console.WriteLine($"  buffer:{ascii}");
+            Console.WriteLine($"");
+        }
+    }
+
+    public void Write(TextWriter sw, string dir, string line, char[] buffer, string ascii)
+    {
+        Print(dir, line, ascii);
+        if (SuppressHeartbeats && Regex.IsMatch(ascii, @"100 Idle Heartbeat"))
+        {
+            if (DisplayHeartbeats)
+                Console.WriteLine($"{new string(' ', dir.Length)}     [SUPPRESSED]");
+        }
+        else
+        {
+            sw.WriteLine(line);
+            sw.Write(buffer);
+            sw.Flush();
+        }
+    }
+
+    public void WriteCtrl(TextWriter sw, string dir, string line)
+    {
+        // Ignore control messages
+        if (DisplayCtrl && !Regex.IsMatch(line, @"^(INFO|CONNECT|PING|PONG|\+OK)"))
+        {
+            Console.WriteLine($"{dir} {line}");
+        }
+
+        sw.WriteLine(line);
+        sw.Flush();
+    }
+}
