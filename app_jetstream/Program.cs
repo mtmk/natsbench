0c130ad3ef720b9ad3c8dedd961f7e4208f60a72
Ziya Suzen <ziya@suzen.net>
2023-09-11 16:17:50 +0100

    Feat2 (#4)
    * feat1-1
    
    * feat2-1

--- app_jetstream/Program.cs
+++ app_jetstream/Program.cs
@@ -1,50 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using Microsoft.Extensions.Logging;
+using NATS.Client.Core;
+using NATS.Client.JetStream;
 
-Span<int> a = new []{1, 2, 3};
+var opts = NatsOpts.Default with { LoggerFactory = new MinimumConsoleLoggerFactory(LogLevel.Error) };
 
-new HashSet<int>();
+await using var nc = new NatsConnection(opts);
+var js = new NatsJSContext(nc);
 
-//
-// using System;
-// using Microsoft.Extensions.Logging;
-// using NATS.Client.Core;
-// using NATS.Client.JetStream;
-//
-// var opts = NatsOpts.Default with { LoggerFactory = new MinimumConsoleLoggerFactory(LogLevel.Error) };
-//
-// await using var nc = new NatsConnection(opts);
-// var js = new NatsJSContext(nc);
-//
-// await js.CreateStreamAsync("orders", subjects: new []{"orders.*"});
-//
-// for (var i = 0; i < 10; i++)
-// {
-//     var ack = await js.PublishAsync($"orders.{i}", new Order(i));
-//     ack.EnsureSuccess();
-// }
-//
-// var consumer = await js.CreateConsumerAsync("orders", "order_processor");
-//
-// Console.WriteLine($"Consume...");
-// await foreach (var msg in consumer.ConsumeAllAsync<Order>(new NatsJSConsumeOpts { MaxMsgs = 10 }))
-// {
-//     var order = msg.Data;
-//     Console.WriteLine($"Processing order {order.OrderId}...");
-//     await msg.AckAsync();
-//     if (order.OrderId == 5)
-//         break;
-// }
-// Console.WriteLine($"Done consuming.");
-//
-// Console.WriteLine($"Fetch...");
-// await foreach (var msg in consumer.FetchAllAsync<Order>(new NatsJSFetchOpts { MaxMsgs = 10 }))
-// {
-//     var order = msg.Data;
-//     Console.WriteLine($"Processing order {order.OrderId}...");
-//     await msg.AckAsync();
-// }
-// Console.WriteLine($"Done fetching.");
-//
-// record Order(int OrderId);
-//
+await js.CreateStreamAsync("orders", subjects: new []{"orders.*"});
+
+for (var i = 0; i < 10; i++)
+{
+    var ack = await js.PublishAsync($"orders.{i}", new Order(i));
+    ack.EnsureSuccess();
+}
+
+var consumer = await js.CreateConsumerAsync("orders", "order_processor");
+
+Console.WriteLine($"Consume...");
+await foreach (var msg in consumer.ConsumeAllAsync<Order>(new NatsJSConsumeOpts { MaxMsgs = 10 }))
+{
+    var order = msg.Data;
+    Console.WriteLine($"Processing order {order.OrderId}...");
+    await msg.AckAsync();
+    if (order.OrderId == 5)
+        break;
+}
+Console.WriteLine($"Done consuming.");
+
+Console.WriteLine($"Fetch...");
+await foreach (var msg in consumer.FetchAllAsync<Order>(new NatsJSFetchOpts { MaxMsgs = 10 }))
+{
+    var order = msg.Data;
+    Console.WriteLine($"Processing order {order.OrderId}...");
+    await msg.AckAsync();
+}
+Console.WriteLine($"Done fetching.");
+
+record Order(int OrderId);
